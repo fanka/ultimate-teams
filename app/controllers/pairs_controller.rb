db5f58c7a93e54c1749495fbc88bff2ab47d4a66
elizabethprins <elizabethprins@gmail.com>
2017-06-09 10:18:37 +0200

    generating and saving pairs, still some hurdles

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -3,13 +3,12 @@ class PairsController < ApplicationController
   before_action :must_be_admin, only: :index
   helper_method :rotate_and_generate_new
 
-
-
   def index
     @pairs = Pair.all
+    @my_pairs1 = Pair.where(student_one_id: current_user.id)
+    @my_pairs2 = Pair.where(student_two_id: current_user.id)
     @users = User.all
     @students_half_1, @students_half_2 = @users.each_slice( (@users.count/2.0).round ).to_a
-
   end
 
   def new
@@ -18,7 +17,6 @@ class PairsController < ApplicationController
 
   def create
     @pair = Pair.new
-
     @pair.save
   end
 
@@ -28,27 +26,25 @@ class PairsController < ApplicationController
 
   def show
     @pair = Pair.find_by(student_one_id: current_user.id) || @pair = Pair.find_by(student_two_id: current_user.id)
-
   end
 
   def update
     @pair = Pair.find(params[:id])
     @pair.update(pair_params)
     @pair.save
-
-
   end
 
   def rotate_and_generate_new
+    Pair.destroy_all
     Pair.daily_rotation
     Pair.generate_pairs
 
+
     respond_to do |format|
-        format.html
+        format.html { redirect_to pairs_path }
         format.js { render :file => "/app/assets/javascripts/pairs.js" }
     end
   end
-
 
 
   private
@@ -60,7 +56,7 @@ class PairsController < ApplicationController
   end
 
   def pair_params
-        params.require(:pair).permit(:student_one, :student_two, :pair_date)
+    params.require(:pair).permit(:student_one, :student_two, :pair_date)
   end
 
 end
--- app/models/pair.rb
+++ app/models/pair.rb
@@ -3,37 +3,35 @@ class Pair < ApplicationRecord
   belongs_to :student_two, class_name: User
   validates_presence_of :student_one, :student_two
 
-@students_half_1, @students_half_2 = User.all.each_slice( (User.all.count/2.0).round ).to_a
+  @students_half_1, @students_half_2 = User.all.each_slice( (User.all.count/2.0).round ).to_a
 
 
-def even_number?(number)
-  number % 2 == 0
-end
+  def even_number?(number)
+    number % 2 == 0
+  end
 
-def total_of_pairs
-  even_number?(User.all.count) ? (User.all.count / 2) : (User.all.count / 2 + 1)
-end
+  def total_of_pairs
+    even_number?(User.all.count) ? (User.all.count / 2) : (User.all.count / 2 + 1)
+  end
 
-def self.daily_rotation
-  x=@students_half_1.pop
-  @students_half_2.push(x)
-  y=@students_half_2.shift
-  @students_half_1.insert(1,y)
-end
+  def self.daily_rotation
+    x=@students_half_1.pop
+    @students_half_2.push(x)
+    y=@students_half_2.shift
+    @students_half_1.insert(1,y)
+  end
 
+  def self.generate_pairs
+    total_of_pairs = (User.count % 2 == 0) ? (User.count / 2) : (User.count / 2 + 1)
+    #Pair.destroy_all
 
-def self.generate_pairs
-  total_of_pairs = (User.count % 2 == 0) ? (User.count / 2) : (User.count / 2 + 1)
-  Pair.destroy_all
-  (0...total_of_pairs).each do |index_of_pair|
-  @pair = Pair.create(student_one: @students_half_1[index_of_pair], student_two: @students_half_2[index_of_pair], pair_date: @pair_date )
+    (0...total_of_pairs).each do |index_of_pair|
+      @pair = Pair.create(student_one: @students_half_1[index_of_pair], student_two: @students_half_2[index_of_pair], pair_date: @pair_date )
+    end
+  end
 
-end
-end
-
-
-
-
-
+  def self.show_today
+    Pair.where(:pair_date == Date.today)
+  end
 
 end
